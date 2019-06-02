14d2fee49b5ffc6befea5585a57ca72ed5edd4de
gomzaemon111 <goki14914@icloud.com>
2019-06-02 23:01:24 +0900

    add list3.c

---
+++ 8/list3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int Item;
+typedef struct node *link;
+struct node {
+  Item item; link next;
+};
+int main(void){
+  link head, a, b, c, d, t, x;
+  a = malloc(sizeof(*a));
+  b = malloc(sizeof(*b));
+  c = malloc(sizeof(*c));
+  d = malloc(sizeof(*d));
+  a->item = 10; b->item = 20; c->item = 30; d->item = 40;
+  a->next = b; b->next = c; c->next = d; d->next = NULL;
+  
+  head = a;
+  x = head->next;
+  t = x->next; x->next = t->next; free(t);
+
+
+  for (t = head; t != NULL; t = t->next){
+    printf("%d\n", t->item);
+  }
+  return 0;
+}
