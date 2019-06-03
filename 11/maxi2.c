b64c49d9cb0e4d3682bbdf15e053fea068b7b5e2
gomzaemon111 <goki14914@icloud.com>
2019-06-04 00:08:11 +0900

    add max

---
+++ 11/maxi2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+typedef char Item;
+
+Item max(Item a[], int l, int r);
+
+int c = 0;
+
+int main(void){
+  Item a[11] = {'T','I','N','Y','E','X','A','M','P','L','E'};
+  Item m;
+  int l=0, r=10;
+  
+  if (l < r) {
+    m = max(a, l, r);
+  } else {
+    m = a[l];
+  }
+  printf("max = %c\n", m);
+  printf("c = %d\n", c);
+  
+  return 0;
+}
+
+Item max(Item a[], int l, int r){
+  Item u, v;
+  int m = (l+r)/2;
+  c++;
+  printf("max(%2d, %2d) is called.\n", l, r);
+  if (l < m) {
+    u = max(a, l, m);
+  } else {
+    u = a[l];
+  }
+  
+  if (m+1 < r) {
+    v = max(a, m+1, r);
+  } else {
+    v = a[r];
+  }
+  if (u > v) {
+    return u;
+  } else {
+    return v;
+  }
+}
